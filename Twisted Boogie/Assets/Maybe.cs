7032c4e698befa5e0dbc7d66aefd293f0a159336
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-03-14 14:57:58 -0400

    Deleted Library folder from Twisted Boogie, created new repo, gitignore now says Library/ instead of [Ll]ibrary

---
+++ Twisted Boogie/Assets/Maybe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maybe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
