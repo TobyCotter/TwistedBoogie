7384eac7e3902651784faa7047e6d6bb2c0c959a
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-03-31 07:32:42 -0400

    Added pivot gameobject for the yellow cube to rotate around.

--- Twisted Boogie/Assets/Scripts/CubeYellow.cs
+++ Twisted Boogie/Assets/Scripts/CubeYellow.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CubeYellow : MonoBehaviour {
 	//Variables
+	public float playerSpeedOffsetZ;
 	private bool setTurnRight = false;
-	
+	private Pivot pivotPoint;
+
 	void Start () {
-		
+		pivotPoint = GameObject.FindObjectOfType<Pivot>();
 	}
 	
 	// Update is called once per frame
@@ -31,7 +33,14 @@ public class CubeYellow : MonoBehaviour {
 
 
 	private void TurnRight(){
-		Vector3 tobyTurn = new Vector3(510f, 1f, 100f);
-		transform.RotateAround(tobyTurn, Vector3.up, 10 * Time.deltaTime);
+		//Vector3 tobyTurn = new Vector3(510f, 1f, 100f);
+		//transform.RotateAround(tobyTurn, Vector3.up, 10 * Time.deltaTime);
+		float pivotX = pivotPoint.transform.position.x;
+		float pivotY = pivotPoint.transform.position.y;
+		float pivotZ = pivotPoint.transform.position.z;
+
+		Vector3 tobyTurn = new Vector3(pivotX, pivotY, pivotZ);
+		transform.RotateAround(tobyTurn, Vector3.up, 300 * Time.deltaTime);
+		//transform.Translate(Vector3.forward * playerSpeedOffsetZ * Time.deltaTime);
 	}//End
 }
