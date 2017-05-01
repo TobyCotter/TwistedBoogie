68e7ee98f8115f9cfd95765358ae67c8fcceab9e
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-05-01 07:38:47 -0400

    Camera had two camera.cs attached to it.

--- Twisted Boogie/Assets/Scripts/CubeTry.cs
+++ Twisted Boogie/Assets/Scripts/CubeTry.cs
@@ -6,8 +6,9 @@ public class CubeTry : MonoBehaviour {
 	//Variables
 	public Transform myPivot;
 	public float turnSpeed;
+	[SerializeField] private float playerZSpeed = 0.5f;
 	private bool rightTurn;
-	private float reach90;
+	private float reach90 = 0.0f;
 	private Vector3 colliderPivot;
 	private float currentY_rotation;
 	private float targetYAngle;
@@ -25,9 +26,18 @@ public class CubeTry : MonoBehaviour {
 
 	void FixedUpdate(){
 		//Turn right
-		if (reach90 < 90.0f && rightTurn) {
-        	reach90 += (turnSpeed * Time.deltaTime);			//We want to keep turning unti we reach 90 degrees
-           	transform.RotateAround(colliderPivot, Vector3.up, turnSpeed * Time.deltaTime);
+		Debug.Log("Reach 90: " + reach90);
+
+		if (reach90 < 90.0f && rightTurn) {						//rightTurn is set in the OnTriggerEnter
+        	reach90 += (turnSpeed * .02f);						//We want to keep turning unti we reach 90 degrees
+        	//reach90 = Mathf.Clamp(reach90, 0.0f, 90.0f);		//Clamp so that we don't finish rotating at a non right angle
+           	transform.RotateAround(colliderPivot, Vector3.up, turnSpeed * .02f);
+           	if(reach90 > 90.0f){
+           		Debug.LogWarning("We are larger than 90!");
+				//Straighten by setting rotation to absolute value
+       			targetYAngle = currentY_rotation + 90.0f;
+        		transform.eulerAngles = new Vector3(0.0f, targetYAngle, 0.0f);
+           	}
         }
 
         //After turning right, straighten
@@ -36,22 +46,20 @@ public class CubeTry : MonoBehaviour {
         	reach90 = 0.0f;										//Reset for next turn
 
         	//Straighten by setting rotation to absolute value
-       		targetYAngle = currentY_rotation + 90.0f;
-        	transform.eulerAngles = new Vector3(0.0f, targetYAngle, 0.0f);
+       		//targetYAngle = currentY_rotation + 90.0f;
+        	//transform.eulerAngles = new Vector3(0.0f, targetYAngle, 0.0f);
         }
 
         //Straight velocity
 		if (!rightTurn){
-            transform.Translate(Vector3.forward * 12 * Time.deltaTime);
+            transform.Translate(Vector3.forward * playerZSpeed * .02f);
 			currentY_rotation = transform.eulerAngles.y;		//Used for strightening out after a turn later on
-			Debug.Log("current eulerAngle y: " + transform.eulerAngles.y);
         }
-	}
+	}//End FixedUpdate
 
 
 	void OnTriggerEnter(Collider collider) {
-    	Debug.Log("We collided with:" + collider);
     	rightTurn = true;
     	colliderPivot = collider.transform.position;
     }//End OnTriggerEnter
-}
+}//End CubeTry
--- Twisted Boogie/Assets/Scripts/PlayerCamera.cs
+++ Twisted Boogie/Assets/Scripts/PlayerCamera.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour {
 	//Variables
 	public Transform targetObj;
-	public float lerpSpeed;
-	public float xOffset;
-	public float yOffset;
-	public float zOffset;
-	//SmoothDamp variables
+	//public float lerpSpeed;
+	//public float xOffset;
+	//public float yOffset;
+	//public float zOffset;
 	public float smoothTime = 0.3f;
 	private Vector3 velocity = Vector3.zero;
+	//private Vector3 camOffset;						//Distance between target object and camera
 	
 	void Start () {
-		
+		//camOffset = targetObj.transform.position - this.transform.position;
 	}
 	
 
 
 	void FixedUpdate(){
-		Vector3 camOffset = new Vector3(xOffset, yOffset, zOffset);
-		Vector3 desiredPos = targetObj.transform.position + camOffset;
+		//Vector3 camOffset = new Vector3(xOffset, yOffset, zOffset);
+		//Vector3 desiredPos = targetObj.transform.position + camOffset;
 
 		//Follow Player
 		//transform.position = Vector3.Lerp(transform.position, desiredPos, (lerpSpeed * Time.deltaTime));
-		transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothTime);
+		//transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothTime);
+		transform.position = Vector3.SmoothDamp(transform.position, targetObj.transform.position, ref velocity, smoothTime);
 
 		//Rotate with player
 		Vector3 relativePos = targetObj.position - transform.position;
 		Quaternion rotation = Quaternion.LookRotation(relativePos);
+		//rotation.y = Mathf.Clamp(rotation.y, 0.0f, 91.0f);	//Used to keep the player from starting out the turn negative
 		transform.rotation = rotation;
-	}
+		//Debug.Log("Rotation y: " + transform.rotation.y);
+	}//End FixedUpdate
 
-	private void LerpCamera(){
-		
-	}
-}
+}//End PlayerCamera
